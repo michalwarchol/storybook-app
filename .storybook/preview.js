897013e912737c63e3028f04fbb85ccd7d79c3e3
Michał Warchoł <imsimilar@gmail.com>
2022-02-25 18:07:16 +0100

    :page_facing_up: Add docs, viewport and a11y addons

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,8 @@
-import {addDecorator} from "@storybook/react";
+import { addDecorator, addParameters } from "@storybook/react";
 import { withConsole } from "@storybook/addon-console";
 import "@storybook/addon-console"; //add console addon
+import { INITIAL_VIEWPORTS } from "@storybook/addon-viewport";
+import { withA11y } from "@storybook/addon-a11y";
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
@@ -9,11 +11,25 @@ export const parameters = {
       color: /(background|color)$/i,
       date: /Date$/,
     },
+    a11y: {
+      element: "#root",
+      config: {},
+      options: {},
+      manual: true,
+    },
   },
   options: {
     storySort: (a, b) =>
-      a[1].kind === b[1].kind ? 0 : a[1].id.localeCompare(b[1].id, undefined, { numeric: true }),
+      a[1].kind === b[1].kind
+        ? 0
+        : a[1].id.localeCompare(b[1].id, undefined, { numeric: true }),
   },
-}
+};
 
-addDecorator((storyFn, context)=> withConsole()(storyFn)(context));
+addDecorator((storyFn, context) => withConsole()(storyFn)(context));
+
+addParameters({
+  viewport: {
+    viewports: INITIAL_VIEWPORTS,
+  },
+});
